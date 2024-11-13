7f1320069ab177dca7fa61aeee3a7ce6390ba785
Elvis M. <u201112959@upc.edu.pe>
2024-11-13 09:22:44 -0500

    refactor: Update project structure and dependencies for new Maven version

---
+++ src/main/java/dev/emrx/gratitude/ChallengeGratitudeMessageApplication.java
@@ -0,0 +1,13 @@
+package dev.emrx.gratitude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeGratitudeMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeGratitudeMessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/emrx/gratitude/ChallengeGratitudeMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emrx.gratitude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeGratitudeMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
